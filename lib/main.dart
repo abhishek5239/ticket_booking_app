2fa96a1969f00c5468762883dfb8040f48d4200c
Abhishek <abhishek.bhadane20@pccoepune.org>
2023-02-19 18:05:56 +0530

    add changes to github

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: true,
-      title: 'experimental branch commit',
+      title: 'chanbges should reflect in github',
       theme: ThemeData(
         primaryColor: primary,
       ),
