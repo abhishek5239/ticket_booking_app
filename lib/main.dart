aa7d920ac233a4d9111c452433efa688529a80e3
Abhishek <abhishek.bhadane20@pccoepune.org>
2023-02-19 14:11:26 +0530

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: true,
-      title: 'flutter demonstartion',
+      title: 'flutter demo',
       theme: ThemeData(
         primaryColor: primary,
       ),
