80b35533b32d9773cd6609a1152bc7a6b9f0979d
Abhishek <abhishek.bhadane20@pccoepune.org>
2023-02-19 16:38:14 +0530

    experimental commit1 change title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: true,
-      title: 'flutter demo',
+      title: 'experimental',
       theme: ThemeData(
         primaryColor: primary,
       ),
